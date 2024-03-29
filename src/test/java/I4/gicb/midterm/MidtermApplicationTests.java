8b0da4fc99ba2e280cb3b1298034f4f177a49ed8
Saingnam <saingnam6@gmail.com>
2024-03-29 16:46:22 +0700

    first commit

---
+++ src/test/java/I4/gicb/midterm/MidtermApplicationTests.java
@@ -0,0 +1,13 @@
+package I4.gicb.midterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
